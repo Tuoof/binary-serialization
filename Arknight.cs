022dbe5bd54ee41d9d87e1cfc665f305170ac9a9
Tuoof <72484719+Tuoof@users.noreply.github.com>
2021-08-30 06:31:34 +0700

    First Pull
    Base Application

---
+++ Arknight.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+// With serialization you can store the state 
+// of an object in a file stream, pass it to
+// a remote network
+
+namespace binary_serialization
+{
+    // Defines that you want to serialize this class
+    [Serializable()]
+    public class Arknight : ISerializable
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Weapon { get; set; }
+        public int ArknightID { get; set; }
+
+        public Arknight() { }
+
+        public Arknight(string name = "No Name",
+            string Class = "No Class",
+            string Weapon = "No Weapon")
+        {
+            this.Name = name;
+            this.Class = Class;
+            this.Weapon = Weapon;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Class: {1}, Weapon: {2} ",
+                Name, Class, Weapon);
+        }
+
+        // Serialization function (Stores Object Data in File)
+        // SerializationInfo holds the key value pairs
+        // StreamingContext can hold additional info
+        // but we aren't using it here
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            // Assign key value pair for your data
+            info.AddValue("Name", Name);
+            info.AddValue("Weight", Class);
+            info.AddValue("Height", Weapon);
+            info.AddValue("AnimalID", ArknightID);
+        }
+
+        // The deserialize function (Removes Object Data from File)
+        public Arknight(SerializationInfo info, StreamingContext ctxt)
+        {
+            //Get the values from info and assign them to the properties
+            Name = (string)info.GetValue("Name", typeof(string));
+            Class = (string)info.GetValue("Weight", typeof(string));
+            Weapon = (string)info.GetValue("Height", typeof(string));
+            ArknightID = (int)info.GetValue("AnimalID", typeof(int));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,100 @@
 ﻿using System;
-
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+ 
+// Used for writing to a file
+using System.IO;
+ 
+// Used to serialize an object to binary format
+using System.Runtime.Serialization.Formatters.Binary;
+ 
+// Used to serialize into XML
+using System.Xml.Serialization;
+ 
 namespace binary_serialization
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            //Arknight bowser = new Arknight("Savage", "Guard", "Hammer");
+            List<Arknight> Characters = new List<Arknight>
+            {
+                new Arknight("Savage", "Guard", "Hammer"),
+                new Arknight("Angelina", "Support", "Staff"),
+                new Arknight("Estelle", "Guard", "No Weapon"),
+                new Arknight("Eunectes", "Defender", "Shield and tomahawk")
+            };
+
+            // Serialize the object data to a file
+            Stream stream = File.Open("ArknightData.dat", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+ 
+            // Send the object data to the file
+            bf.Serialize(stream, Characters);
+            stream.Close();
+ 
+            // Read object data from the file
+            stream = File.Open("ArknightData.dat", FileMode.Open);
+            bf = new BinaryFormatter();
+ 
+            Characters = (List<Arknight>)bf.Deserialize(stream);
+            stream.Close();
+ 
+            foreach (Arknight a in Characters)
+            {
+                Console.WriteLine(a.ToString());
+            }
+            
+            /*
+            // XmlSerializer writes object data as XML
+            XmlSerializer serializer = new XmlSerializer(typeof(Arknight));
+            using (TextWriter tw = new StreamWriter(@"bowser.xml"))
+            {
+                //serializer.Serialize(tw, bowser);
+            }
+ 
+            // Delete bowser data
+            bowser = null;
+ 
+            // Deserialize from XML to the object
+            XmlSerializer deserializer = new XmlSerializer(typeof(Arknight));
+            TextReader reader = new StreamReader(@"bowser.xml");
+            object obj = deserializer.Deserialize(reader);
+            bowser = (Arknight)obj;
+            reader.Close();
+ 
+            Console.WriteLine(bowser.ToString());
+            */
+            
+            /*
+            using (Stream fs = new FileStream(@"animals.xml",
+                FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                XmlSerializer serializer2 = new XmlSerializer(typeof(List<Arknight>));
+                serializer2.Serialize(fs, Characters);
+            }
+ 
+            // Delete list data
+            Characters = null;
+ 
+            // Read data from XML
+            XmlSerializer serializer3 = new XmlSerializer(typeof(List<Arknight>));
+ 
+            using (FileStream fs2 = File.OpenRead(@"animals.xml"))
+            {
+                Characters = (List<Arknight>)serializer3.Deserialize(fs2);
+            }
+ 
+ 
+            foreach (Arknight a in Characters)
+            {
+                Console.WriteLine(a.ToString());
+            }
+ 
+            Console.ReadLine();
+            */
         }
     }
 }
